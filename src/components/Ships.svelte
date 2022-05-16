d36aa3c2392d3f0d0d58388103259990f3a841fc
douryne <douryne.1st@gmail.com>
2022-05-16 12:18:36 +0500

    refactor: error and loading checking

--- src/components/Ships.svelte
+++ src/components/Ships.svelte
@@ -11,6 +11,8 @@
   let ships;
   const shipsCount = query(GET_SHIPS_COUNT);
   let pagesCount = 0;
+  let loading;
+  let error;
   
   onMount(() => {
     const pageLC = JSON.parse(localStorage.getItem('page'));
@@ -27,11 +29,24 @@
     });
   }
 
-  $: if (!$shipsCount.loading) {
-    pagesCount = Math.ceil($shipsCount.data.shipsResult.result.totalCount / maxElemsOnPage);
+  $: {
+    if ($ships.loading || $shipsCount.loading) {
+      loading = true;
+    } else {
+      loading = false;
+    }
   }
-  
-  $: if (!$ships.loading) {
+
+  $: {
+    if ($ships.error || $shipsCount.error) {
+      error = true;
+    } else {
+      error = false;
+    }
+  }
+
+  $: if (!loading && !error) {
+    pagesCount = Math.ceil($shipsCount.data.shipsResult.result.totalCount / maxElemsOnPage);
     localStorage.setItem('page', JSON.stringify(page));
   }
 </script>
@@ -39,8 +54,8 @@
 <h1>Ships</h1>
 
 <div>
-  <MyButton on:click={() => page -= 1} disabled={page === 1 || $ships.loading}>Prev</MyButton>
-  <MyButton on:click={() => page += 1} disabled={page === pagesCount || $ships.loading}>Next</MyButton>
+  <MyButton on:click={() => page -= 1} disabled={page === 1 || loading || error}>Prev</MyButton>
+  <MyButton on:click={() => page += 1} disabled={page === pagesCount || loading || error}>Next</MyButton>
 </div>
 
 <div class="ships-list">
